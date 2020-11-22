fac074896466615b067724a76cb93cefa8f6ac0a
kim-seonkyu <64760329+kim-seonkyu@users.noreply.github.com>
2020-11-22 18:52:40 +0900

    stt + nlp + html + db(select)

---
+++ voicehelper.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import gspeech
+import time
+import konlpy
+import os
+import webbrowser
+import pymysql
+
+from konlpy.tag import Okt
+
+def main():
+    gsp = gspeech.Gspeech()
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    webbrowser.open('file://' + os.path.join(BASE_DIR, 'voicehelper.html'))
+
+    while True:
+        # 음성 인식 될때까지 대기 한다.
+        stt = gsp.getText()
+        okt = Okt()
+        nlp2 = okt.morphs(stt)
+        # mysql select start
+        conn = pymysql.connect(host="localhost", user="root", password="root", db="voicehelper", charset="utf8")
+        try:
+            curs = conn.cursor()
+            sql = "select * from command"
+            curs.execute(sql)
+            rows = curs.fetchall()
+            for row in rows:
+                print(row)
+        finally:
+            conn.close()
+        # mysql select finish
+        time.sleep(0.01)
+        if stt is None:
+            break
+        print(stt)
+        print(nlp2)
+        time.sleep(0.01)
+        if ('끝내자' in stt):
+            break
+
+
+if __name__ == '__main__':
+    main()
+# [END speech_transcribe_streaming_mic]
